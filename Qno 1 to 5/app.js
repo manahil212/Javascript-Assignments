d1395f73c4323d4d063ae824b98e03da6abd38c9
manahil212 <manahilwasi101@gmail.com>
2025-09-01 09:34:30 -0700

    chap alert done

---
+++ Qno 1 to 5/app.js
@@ -0,0 +1,13 @@
+// Q1 Write a script to greet your website visitor using JS alert box.
+alert("Hello! welcome to my website");
+//  Q2 Write a script to display following message on your web page.
+ alert("Error! Please enter a valid password");
+//  Q3 Write a script to display following message on your web page: (Hint : Use line break)
+alert("Welcome to JS land...\nhappy coding!")
+// Q4 Write a script to display following messages in sequence:
+// first alert
+alert("Welcome to JS land...")
+// second alert
+alert("Happy coding!")
+//Q5 Generate the following message through browser’s developer console:
+console.log("Hello...I can run JS through my web browser console")
