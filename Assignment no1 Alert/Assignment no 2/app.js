bf983aed469fffbfd46875b60cc004c029116dac
manahil212 <manahilwasi101@gmail.com>
2025-09-08 00:44:17 -0700

    assignment 2 done

---
+++ Assignment no1 Alert/Assignment no 2/app.js
@@ -0,0 +1,92 @@
+// Question # 1 Declare a variable called username?
+var username = ("")
+console.log("username")
+// Question # 2 Declare a variable called myName & assign to it a string that represents your Full Name?
+var MyName = ("Manahilwasi")
+console.log(MyName)
+// Question # 3 Write script to
+// a) Declare a JS variable, titled message.
+var titlemessege = ("this is my JS variable")
+console.log(titlemessege)
+// b) Assign “Hello World” to variable message?
+var messege = ("Hello World")
+// c) Display the message in alert box.
+alert("Hello World")
+// Question # 4 Write a script to save student’s bio data in JS variables andshow the data in alert boxes.
+var studentName = ("johne doe")
+alert(studentName)
+// age
+var studentage = ("15 years old")
+alert(studentage)
+// certified
+var studentcertificate = ("Certified by Mobile Application Development")
+alert(studentcertificate)
+//Question # 5 Write a script to display the following alert using one JS variable.
+ var pizza   = ("PIZZA\nPIZZ\nPIZ\nPI\nP")
+ alert(pizza)
+//  Question # 6 Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+var email   = (" manahil@example.com).")
+alert(email)
+// using string concetentation
+var email   = ("My email address is"  + email)
+alert(email)
+// Question # 7 Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+var book = (" A smarter way to learn JavaScript")
+// show messege in alert box
+alert("I am trying to learn from the book" + book)
+//Question # 8 Write a script to display this in browser through JS
+document.write("Yah! I can write the HTML content through javascript")
+// Question # 9 Store following string in a variable and show in alert andbrowser through JS
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// on browser
+document.write("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+// show in alert
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+// show in browser (on the page)
+document.write("END")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var studentName = ("tayyaba ali")
+// var studentNumber = ("32177896667")
+// var studentSubject = ("english")
+// console.log(studentSubject)
+// variable vary kerna means change kerna hai
+// studentName = "yumna"
+// reassignmnet 
+// console.log(studentName)
+
+// alert("student name")
+// document.write("student name")
