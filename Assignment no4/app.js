833c70fadfec02e07049e78850c9bd76a372929d
manahil212 <manahilwasi101@gmail.com>
2025-09-08 10:22:32 -0700

    done

---
+++ Assignment no4/app.js
@@ -0,0 +1,8 @@
+// question 1 Declare 3 variables in one statement.
+let name;age;course
+// question 2 Declare 5 legal & 5 illegal variable names.
+// legal variable
+alertandlog ;age ;_name ;$price ;userName
+// illegal variable
+var ;123hello ;user Name ;user-name ;@user
+// queation 3 Display this in your browser
